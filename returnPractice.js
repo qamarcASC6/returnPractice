618f37f25ea028a9ca7578cd463c0218f753bee3
qamarcASC6 <qamarchisti292@gmail.com>
2019-07-12 06:48:11 -0700

    Creates funtion with return value

---
+++ returnPractice.js
@@ -0,0 +1,13 @@
+function someFunc() {
+//return syntax
+return "hello world";
+console.log("will this code prnt");
+//return 5;
+}
+//by defult all JS functions return a vaule 
+//if the return value is not specified the function returns undefined
+
+
+const result = someFunc();
+
+console.log(result);
